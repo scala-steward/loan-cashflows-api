12bd1f9656bdac7c0f6b74575e56e96fdb4020bc
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-01-18 20:40:48 +0100

    Update akka-http, akka-http-spray-json, ... to 10.2.3 (#12)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   private object Versions {
     val cats = "2.3.1"
-    val akkaHttp = "10.2.2"
+    val akkaHttp = "10.2.3"
     val akka = "2.6.11"
     val scalaTest = "3.2.3"
   }
