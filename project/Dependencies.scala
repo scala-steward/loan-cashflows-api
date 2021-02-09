309370dbdb76fcb2ebb37ded2bed74ead0bc8b87
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-02-10 00:48:15 +0100

    Update cats-core to 2.4.0 (#15)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   private object Versions {
-    val cats = "2.3.1"
+    val cats = "2.4.0"
     val akkaHttp = "10.2.3"
     val akka = "2.6.12"
     val scalaTest = "3.2.3"
