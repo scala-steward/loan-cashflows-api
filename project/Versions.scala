a372c9d12d3144d70670183c5816de82f6386a6d
Scala Steward <me@scala-steward.org>
2021-04-01 08:16:49 +0200

    Update scalatest to 3.2.7

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,6 +3,6 @@ object Versions {
   val cats = "2.5.0"
   val akkaHttp = "10.2.4"
   val akka = "2.6.13"
-  val scalaTest = "3.2.6"
+  val scalaTest = "3.2.7"
   val scalafixOrganizeImports = "0.5.0"
 }
