b257ef3aaaf27a7f3c09c4ce5749a47b6aa5c3eb
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-10-15 14:22:00 +0200

    Update akka-stream, akka-stream-testkit, ... to 2.6.17 (#73)

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ object Versions {
   val scala = "2.13.6"
   val cats = "2.6.1"
   val akkaHttp = "10.2.6"
-  val akka = "2.6.16"
+  val akka = "2.6.17"
   val scalaTest = "3.2.10"
   val scalafixOrganizeImports = "0.5.0"
 }
