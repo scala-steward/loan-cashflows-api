07ce110af9fbfb8a207e20333bba625cdcd58d8f
Scala Steward <me@scala-steward.org>
2022-02-14 18:37:49 +0100

    Update akka-http, akka-http-spray-json, ... to 10.2.8

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   val scala = "2.13.8"
   val cats = "2.7.0"
-  val akkaHttp = "10.2.7"
+  val akkaHttp = "10.2.8"
   val akka = "2.6.18"
   val scalaTest = "3.2.11"
   val scalafixOrganizeImports = "0.6.0"
