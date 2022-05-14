bb81ae26ea890042a3037919ff575fc04db9b78c
Scala Steward <me@scala-steward.org>
2022-05-14 18:22:13 +0200

    Reformat with scalafmt 3.5.3

--- src/main/scala/it/mdtorelli/cashflows/api/route/RootRoute.scala
+++ src/main/scala/it/mdtorelli/cashflows/api/route/RootRoute.scala
@@ -3,10 +3,10 @@ package it.mdtorelli.cashflows.api.route
 import akka.http.scaladsl.server.{Directives, Route}
 
 object RootRoute {
-  def apply: Route = new RootRoute().route
+  def apply: Route = new RootRoute.route
 }
 
-final class RootRoute private () extends Directives {
+final class RootRoute private extends Directives {
   lazy val route: Route =
     pathSingleSlash {
       get {
--- src/main/scala/it/mdtorelli/cashflows/financial/convergence/ConvergenceFunctions.scala
+++ src/main/scala/it/mdtorelli/cashflows/financial/convergence/ConvergenceFunctions.scala
@@ -28,8 +28,8 @@ object ConvergenceFunctions {
 
           while (f0.abs > tolerance) {
             val f1 = f(x0 + f0)
-            val g = (f1 / f0) - 1
-            x0 = x0 - (f0 / g)
+            val g = f1 / f0 - 1
+            x0 = x0 - f0 / g
             f0 = f(x0)
           }
 
@@ -53,8 +53,8 @@ object ConvergenceFunctions {
             if (f0.abs <= tolerance) x0.toDecimal
             else {
               val f1 = f(x0 + f0)
-              val g = (f1 / f0) - 1
-              val x1 = x0 - (f0 / g)
+              val g = f1 / f0 - 1
+              val x1 = x0 - f0 / g
               step(x1, f(x1))
             }
 
