bd7200596c0685ceb8fb4fa36c2d15d4532c02bd
Scala Steward <scala_steward@virtuslab.com>
2022-07-31 13:11:08 +0000

    Update scalatest to 3.2.13

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,6 +3,6 @@ object Versions {
   val cats = "2.8.0"
   val akkaHttp = "10.2.9"
   val akka = "2.6.19"
-  val scalaTest = "3.2.12"
+  val scalaTest = "3.2.13"
   val scalafixOrganizeImports = "0.6.0"
 }
