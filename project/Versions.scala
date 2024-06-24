6769475d903deff10420b1ce69929630bbcf2881
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-06-24 15:07:08 +0200

    Update scalatest to 3.2.19 (#222)

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,6 +3,6 @@ object Versions {
   val cats = "2.12.0"
   val akkaHttp = "10.2.10"
   val akka = "2.6.20"
-  val scalaTest = "3.2.18"
+  val scalaTest = "3.2.19"
   val scalafixOrganizeImports = "0.6.0"
 }
